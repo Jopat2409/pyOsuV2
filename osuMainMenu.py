be55208723e88d9e3ae195a698383e9c46624308
Jopat2409 <joantpat@gmail.com>
2021-03-29 02:43:57 +0100

    Updated render method

--- osuMainMenu.py
+++ osuMainMenu.py
@@ -2,6 +2,8 @@ import logging
 import sys
 import pygame
 import pygame.gfxdraw
+import pygame.font
+import pygamePosLib
 
 import osuBeatmapGamestate
 import osuGlobals
@@ -17,16 +19,39 @@ class GAMESTATE_MainMenu:
     def __init__(self, gamestateManager):
     
         self.gamestateManager = gamestateManager
+
+        self.largeFont = pygame.font.SysFont('Comic Sans MS', 200)
+        self.osuText = self.largeFont.render("OSU!", True, (255,255,255))
         
         self.inputMap = {1:self.play,3:sys.exit}
         self.eventMap = {}
+        self.tempCanvas = pygame.Surface((int(osuGlobals.osuSettings["Width"]),int(osuGlobals.osuSettings["Height"])), pygame.SRCALPHA)
+
+        self.backgroundIMG = pygame.image.load("assets/background.jpg")
+        self.backgroundIMG = pygame.transform.scale(self.backgroundIMG, (osuGlobals.osuSettings["Width"], osuGlobals.osuSettings["Height"]))
+        
+
+
+        self.cHeight = math.ceil(int(osuGlobals.osuSettings["Height"]) / 3)
+
+        self.tempX = math.ceil(int(osuGlobals.osuSettings["Width"]) / 2)
+        self.tempY = math.ceil(int(osuGlobals.osuSettings["Height"]) / 2)
+        self.hover = False
         logging.info("Initialized main menu gamestate")
 
 
-    def update(self):
+    def update(self, events):
 
-        # temporary text menu to get the program up and running
-        pass
+        mouseX, mouseY = pygame.mouse.get_pos()
+
+        if(mouseX < (self.tempX + self.cHeight) and mouseX > (self.tempX - self.cHeight)) and (mouseY < (self.tempY + self.cHeight) and mouseY > (self.tempY - self.cHeight)):
+            self.hover = True
+        else:
+            self.hover =False
+        if "mb1" in events and self.hover:
+            self.play()
+        
+
 
         
     def play(self):
@@ -37,10 +62,18 @@ class GAMESTATE_MainMenu:
 
     def draw(self):
 
-        tempCanvas = pygame.Surface((math.ceil(int(osuGlobals.osuSettings["Height"]) / 2),math.ceil(int(osuGlobals.osuSettings["Height"]) / 2)))
-        pygame.gfxdraw.filled_circle(tempCanvas,0,0,math.ceil(int(osuGlobals.osuSettings["Height"]) / 4), (100,100,100))
+        self.tempCanvas.blit(self.backgroundIMG, (0,0))
+        
+        if self.hover:
+            pygame.gfxdraw.filled_circle(self.tempCanvas,self.tempX, self.tempY, self.cHeight+20, (200,200,200))
+        pygame.gfxdraw.filled_circle(self.tempCanvas,self.tempX, self.tempY, self.cHeight+15, (255,255,255))
+        pygame.gfxdraw.filled_circle(self.tempCanvas,self.tempX, self.tempY, self.cHeight, (255,102,170))
 
-        return tempCanvas
+        pygamePosLib.blitCenter(self.osuText, self.tempCanvas)
+
+        
+
+        return self.tempCanvas
         
 
     def clear(self):
